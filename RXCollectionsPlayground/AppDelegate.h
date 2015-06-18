e4cb9137225f15acf4667c68dcbc0bb5d2df8529
Dan Stepanov <danhstepanov@gmail.com>
2015-06-18 02:30:14 -0700

    Initial Commit

---
+++ RXCollectionsPlayground/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RXCollectionsPlayground
+//
+//  Created by Daniel Stepanov on 6/18/15.
+//  Copyright (c) 2015 Daniel Stepanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RXCollectionsPlayground/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RXCollectionsPlayground
+//
+//  Created by Daniel Stepanov on 6/18/15.
+//  Copyright (c) 2015 Daniel Stepanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
